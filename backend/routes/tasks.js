48489efcce48fa428b0a9fa22673a90c686f5928
catrinalim <catrinalim@ufl.edu>
2024-12-18 20:25:12 -0500

    created controllers

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,39 +1,28 @@
 const express = require('express')
-const Task = require('../models/taskModel')
+const {
+    getTasks,
+    getTask,
+    createTask,
+    deleteTask,
+    updateTask
+} = require('../controllers/taskController')
 
 const router = express.Router()
 
 // GET all tasks
-router.get('/', (req, res) => {
-    res.json({msg: 'GET all tasks'})
-})
+router.get('/', getTasks)
 
 // GET a single task
-router.get('/:id', (req, res) => {
-    res.json({msg: 'GET a single task'})
-})
+router.get('/:id', getTask)
 
 // POST a new task
-router.post('/', async (req, res) => {
-    const {title, weight} = req.body
-
-    try {
-        const task = await Task.create({title, weight})
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createTask)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a task'})
-})
+router.delete('/:id', deleteTask)
 
 // UPDATE a task
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a task'})
-})
+router.patch('/:id', updateTask)
 
 
 module.exports = router
