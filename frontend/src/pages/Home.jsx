7c26db6ba9879887d1d57dbe64bf4e6ed9cf8a59
catrinalim <catrinalim@ufl.edu>
2024-12-19 13:12:23 -0500

    edited frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import { useEfect, useState } from 'react'
+
 const Home = () => {
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            
+        }
+    }, [])
+
     return(
         <div className="home">
             <h2>Home</h2>
