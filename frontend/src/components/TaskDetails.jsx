821e545b7b527d48068a4b86728de47d8b01488c
catrinalim <catrinalim@ufl.edu>
2024-12-20 18:07:37 -0500

    created delete functionality

--- frontend/src/components/TaskDetails.jsx
+++ frontend/src/components/TaskDetails.jsx
@@ -1,9 +1,25 @@
+import { useTasksContext } from "../hooks/useTasksContext"
+
 const TaskDetails = ({ task }) => {
+    const { dispatch } = useTasksContext()
+
+    const handleClick = async () => {
+        const response = await fetch('api/tasks/' + task._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_TASK', payload: json})
+        }
+    }
+
     return (
         <div className="task-details">
             <h4>{task.title}</h4>
             <p><strong>Weight: </strong>{task.weight}</p>
             <p>{task.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
