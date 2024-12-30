30169b421d057151b26a458bc668d658b1c73ff0
catrinalim <catrinalim@ufl.edu>
2024-12-30 14:30:26 -0500

    set up user routes, controller, and model

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const taskRoutes = require('./routes/tasks')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/tasks', taskRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
